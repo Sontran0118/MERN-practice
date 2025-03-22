51597891866caad45590df7eea98de3ed091d3a0
sontran <xuansontran18012005@gmail.com>
2025-03-22 14:37:07 -0400

    backend

--- backend/controllers/workout.controller.js
+++ backend/controllers/workout.controller.js
@@ -31,6 +31,22 @@ const getWorkout = async(req,res) =>{
 //create a new workout
 const createWorkout = async (req, res)=>{
     const {title, load, reps} = req.body
+
+    let emptyFields =[]
+    if(!title){
+        emptyFields.push('title')
+    }
+    if(!load){
+        emptyFields.push('load')
+    }
+    if(!reps){
+        emptyFields.push('reps')
+    }
+
+    if(emptyFields.length > 0){
+        return res.status(400).json({error: 'Please fill in all fields',emptyFields})
+    }
+
     //add doc to db
     try {
         const workout = await Workout.create({title,load,reps})
@@ -41,16 +57,17 @@ const createWorkout = async (req, res)=>{
 
 }
 
-//delelte a workout
+//delete a workout
 const deleteWorkout = async (req,res)=>{
     const {id} = req.params
     if(!mongoose.Types.ObjectId.isValid(id)){
         res.status(404).json({error: 'no such workout'})
     }
-    const workout = await Workout.findOneAndDelete(id)
+    const workout = await Workout.findByIdAndDelete(id)
     //add doc to db
     if(workout){
-        res.status(200).json({ workout: workout, message: ' has been deleted'})
+        console.log({workout_id: workout._id, title: workout.title, message: 'this data has been deleted' })
+        res.status(200).json(workout)
     }else{
         return res.status(404).json({error: 'No such workout exist'})
     }
@@ -61,21 +78,41 @@ const deleteWorkout = async (req,res)=>{
 
 //update a workout
 const updateWorkout = async (req,res)=>{
+    
+
+    //input error handling
+    const {title, load, reps} = req.body
+    let emptyFields =[]
+    if(!title){
+        emptyFields.push('title')
+    }
+    if(!load){
+        emptyFields.push('load')
+    }
+    if(!reps){
+        emptyFields.push('reps')
+    }
+
+    if(emptyFields.length > 0){
+        return res.status(400).json({error: 'Please fill in all fields',emptyFields})
+    }
+
+
+
    const {id} = req.params
 
    if(!mongoose.Types.ObjectId.isValid(id)){
     return res.status(404),json({error: 'no such workout exists'})
    }
    
-   const workout = await Workout.findOneAndUpdate({_id : id},{
-    ...req.body
-   })
+   const workout = await Workout.findOneAndUpdate({_id : id},
+    {...req.body}, {new: true})
 
    if(!workout){
     res.status(404),json({error: 'no such workout exists'})
    } else{
     const newWorkout = await Workout.findById(id)
-    res.status(200).json({ title: workout.title, message: 'New content after being updated', newworkout: newWorkout})
+    res.status(200).json(newWorkout)
    }
 
 
